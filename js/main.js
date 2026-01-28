76c772fe0931c99c1ac0f5ba49a8507f2bbcafb4
adopra4 <adopra4@gmail.com>
2026-01-28 23:59:54 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -51,6 +51,25 @@ async function loadSongs() {
 function getDefaultSongs() {
     return [
         {
+            id: 'song1',
+            title: 'Midnight Dreams',
+            artist: 'Luna Eclipse',
+            cover: 'https://images.unsplash.com/photo-1493225255756-d9584f8606e9?w=400',
+            audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+            lyrics: `00:00:05|Dreaming in the midnight hour
+00:00:12|Stars above are shining bright
+00:00:20|Whispers of a distant power
+00:00:28|Guiding through the night
+00:00:35|Moonlight dances on the water
+00:00:42|Reflections of our love
+00:00:50|Time stands still, moments captured
+00:00:58|Like stars from above
+00:01:10|Midnight dreams take me away
+00:01:18|To a place where we can stay
+00:01:25|Forever in this endless night
+00:01:35|Holding on to morning light`
+        },
+        {
             id: 'song2',
             title: 'Neon City',
             artist: 'Cyber Pulse',
@@ -68,7 +87,7 @@ function getDefaultSongs() {
 00:01:12|Paint the darkness with your light
 00:01:20|Neon city, hear my call
 00:01:28|Don't let me fall`
-        },
+        }
     ];
 }
 
